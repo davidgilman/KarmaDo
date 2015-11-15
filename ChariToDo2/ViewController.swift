32fa1a8ffe45e78f89996ae6d74a0215f39a8784
Monte Thakkar <mthakkar@mail.sfsu.edu>
2015-11-14 16:46:49 -0800

    Initial Commit

---
+++ ChariToDo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChariToDo2
+//
+//  Created by Monte's Pro 13" on 11/14/15.
+//  Copyright © 2015 Monte Thakkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
